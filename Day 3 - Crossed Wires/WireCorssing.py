786c13ccc2756809a4d489544e494f13769b3975
Roberto H <robertoh89@gmail.com>
2020-02-17 15:53:11 -0800

    Day 3 complete. Still need to refactor. Ugly code

--- Day 3 - Crossed Wires/WireCorssing.py
+++ Day 3 - Crossed Wires/WireCorssing.py
@@ -1,4 +1,12 @@
 from collections import namedtuple
+import os
+
+BASE_PATH = "./Day 3 - Crossed Wires/"
+
+WireA = BASE_PATH + "WireA.txt"
+WireB = BASE_PATH + "WireB.txt"
+TestWireA = BASE_PATH + "TestWireA.txt"
+TestWireB = BASE_PATH + "TestWireB.txt"
 
 class Movement():
     def __init__(self, step):
@@ -50,23 +58,33 @@ def readInput(filePath):
     data = file.read().split(',')
     return data
     
-wireA = "WireA.txt"
-filePathB = "WireB.txt"
-testWireA = "TestWireA.txt"
-testWireB = "TestWireB.txt"
+
 
 if __name__ == "__main__":
-    WireA = WirePath(readInput(testWireA))
-    WireB = WirePath(readInput(testWireB))
-    diff = []
+    print("Working on dir: {}".format(os.getcwd()))
+    WireA = WirePath(readInput(WireA))
+    WireB = WirePath(readInput(WireB))
+    distancesFromOrigin = []
+    crossSectionIndexes = []
+    crossSectionsWireLength = []
+    o = [27726, 24752, 25018, 34482, 91744, 88346, 12936, 93100, 13588, 93100, 103932, 104964, 62774, 62890, 65384, 65384, 109252, 109612, 108292, 108832, 114712, 113110, 116094, 113784, 113784, 126648, 126990, 72202, 73068, 116004, 117300, 119868, 120014, 120460, 121222, 120346, 152852, 153480, 158918, 79718, 79580, 156474, 78262, 77574, 108936, 166338, 144794, 144794, 113642, 155712, 187280, 108156, 107942, 188878, 184664, 106408, 106408, 184664, 189710, 189710, 186924, 186924, 124964, 124964, 123876, 123876, 127900, 130936]
+    print(min(o))
     for spaceA in WireA.Coordinates:
-        for spaceB in WireB.Coordinates:
-            if spaceA == spaceB:
-                print("Same Space: {}".format(spaceB))
-                diff.append(abs(spaceA.x + spaceA.y))
-    print(diff)
-    diff.pop(0)
-    print("Min Cross Section Distance: {}".format(min(diff)))
+        try:
+            wireBIndex = WireB.Coordinates.index(spaceA)
+            wireAIndex = WireA.Coordinates.index(spaceA)
+            crossSectionIndexes.append([wireAIndex,wireBIndex])
+            totalWireLengthPath = wireAIndex + wireBIndex
+            crossSectionsWireLength.append(totalWireLengthPath)
+            differenceFromOrigin = (abs(spaceA.x) + abs(spaceA.y))
+            print("Same Space: {} , Distance: {}, Wire A Length: {}, Wire B Length: {}, Total Length: {}".format(spaceA, differenceFromOrigin, wireAIndex, wireBIndex, totalWireLengthPath))
+            distancesFromOrigin.append(differenceFromOrigin)
+        except:
+            pass
+    print("Distances from Origin: {}".format(distancesFromOrigin))
+    print("Total Lengths from cross section: {}".format(crossSectionsWireLength))
+    distancesFromOrigin.pop(0)
+    print("Min Cross Section Distance: {}".format(min(distancesFromOrigin)))
     #for space in WireA.Coordinates:
     #WireA.PrintMovements()
     ## Draw WireA Path
