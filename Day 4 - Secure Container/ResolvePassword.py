7ad298ae2ace01fbddeb7de94e695fb6480e7b98
Roberto Himmelbauer P <robertohimmelbauerp@Robertos-MacBook-Pro.local>
2020-02-19 12:06:04 -0800

    Refactor code for readability and modularity. Created functions and renamed variables. Next step Create a Class

--- Day 4 - Secure Container/ResolvePassword.py
+++ Day 4 - Secure Container/ResolvePassword.py
@@ -1,25 +1,62 @@
 def CheckStringDuplicate(num):
     for c in num:
-        if num.count(c) > 1 and (num.count(c) % 2) == 0:
+        if num.count(c) > 1:
             return True
     return False
 
+def CreateIntegerListBewteenRanges(start, end):
+    return list(range(start,end))
+
+def CreateIntegerStingListFromIntgers(integers):
+    return [ str(num) for num in integers ]
+
+def DoesIntegerContainZero(integer):
+    if '0' in (str(integer)):
+        return True
+    else:
+        return False
+
+def FilterIntegresWithZero(integers):
+    return [ num for num in integers if DoesIntegerContainZero(num) == False ]
+
+def IsFollingIntegerGreaterOrEqualThanCurrent(current, following):
+    if following >= current:
+        return True
+    else:
+        return False
+
+def TransformIntegerToList(integer):
+    return list(map(int,str(integer)))
+
+def ProcessIntegerForAcendingNumbers(integer):
+    numbersInInteger = TransformIntegerToList(integer)
+    for index in range(0, len(numbersInInteger) - 1):
+        if IsFollingIntegerGreaterOrEqualThanCurrent(numbersInInteger[index], numbersInInteger[index + 1]) == False:
+            return False
+
+    return True
+
+def FilterAcendingIntegerSequence(integers):
+    acendingIntegerFilter = []
+    for integer in integers:
+        if ProcessIntegerForAcendingNumbers(integer):
+            acendingIntegerFilter.append(integer)
+    return acendingIntegerFilter
+
+def FilterDuplicateIntegers(integers):
+    integersWithDuplicates = []
+    for integer in integers:
+        if CheckStringDuplicate(str(integer)):
+            integersWithDuplicates.append(integer)
+    return integersWithDuplicates
 
 if __name__ == "__main__":
-    holeNumbers = list(range(254032,789860,1))
-    holeNumbersAsStrings = [ str(num) for num in holeNumbers]
-    zeroFilter = [ num for num in holeNumbersAsStrings if('0' in num) == False ]
-    increasFilter = zeroFilter.copy()
-    hasDuplicate = []
-    for num in zeroFilter:
-        for index in range(0,len(num)-1):
-            if int(num[index + 1]) < int(num[index]):
-                increasFilter.remove(num)
-                break
-    for num in increasFilter:
-        if(CheckStringDuplicate(num)):
-            hasDuplicate.append(num)
-    print("Remeber git")
-    print("lastFilter: {}".format(hasDuplicate))
-    print("Guesses Length: {}\nnoZeros Length: {},\nincreaseFilter: {}\nhasDuplicates: {}".format(len(holeNumbersAsStrings), len(zeroFilter),len(increasFilter),len(hasDuplicate)))
+    integers = CreateIntegerListBewteenRanges(254032,789860)
+    #integersAsStrings = CreateIntegerStingListFromIntgers(integers)
+    #integersWithoutZeros = FilterIntegresWithZero(integers)
+    integersWithAcendingOrder = FilterAcendingIntegerSequence(integers)
+    integersWithDuplicates = FilterDuplicateIntegers(integersWithAcendingOrder)
+    
+    
+    print("Integers Length: {}\nIntegers With Acending Order: {}\nIntegers With Duplicates: {}".format(len(integers),len(integersWithAcendingOrder),len(integersWithDuplicates)))
     pass
